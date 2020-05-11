452b96752647d1afb2693fef3bfe5aaecac49ef9
aong <yxwzaxns@gmail.com>
2020-05-11 16:31:54 +0800

    completed nav

--- tutorials/static/js/init.js
+++ tutorials/static/js/init.js
@@ -10,36 +10,43 @@ window.addEventListener('load',()=>{
     document.querySelector("#doc").innerHTML = markdown
     hljs.initHighlighting()
 
-    const p = `<div id="head">
-        <a class="back" href="#"><div>上一节</div></a>
-        <a class="menu" href="/tutorials/"><div>目录</div></a>
-        <a class="next" href="#"><div>下一节</div></a>
-    </div>`
-    const nav = createElementFromHTML(p)
-    const doc = document.querySelector("#doc")
-    const docp = doc.parentNode
-
-    docp.insertBefore(nav,doc)
-
-    const currentPage = new RegExp(/_(.*)\.html/).exec(location.pathname)[1]
-    console.log(currentPage);
-    const no = pages.indexOf(currentPage)
-    console.log(no);
-    if(no>=0){
-        if(no==0){
-            document.querySelector("#head .back").href = '/tutorials/index.html'
-        }
-        document.querySelector("#head .back").href = location.pathname.replace(/\d+_(.*)\.html/,`${no-1}_${pages[no-1]}.html`)
-        if(no!=pages.length-1){
-            document.querySelector("#head .next").href = location.pathname.replace(/\d+_(.*)\.html/,`${no+1}_${pages[no+1]}.html`)
-        }else{
-            document.querySelector("#head .next div").innerHTML = "待更新"
-        }
-    }
+    createNav()
 })
 
 function createElementFromHTML(htmlString) {
   var div = document.createElement('div');
   div.innerHTML = htmlString.trim();
   return div.firstChild;
+}
+
+function createNav(){
+    let no = new RegExp(/(\d+)_.*\.html/).exec(location.pathname)?.[1]
+    if(no==undefined) return
+    no = parseInt(no)
+    const doc = document.querySelector("#doc")
+
+    const p = `<div id="head" style="width:${doc.style.width}">
+        <a class="back" href="#"><div>上一节</div></a>
+        <a class="menu" href="/tutorials/"><div>目录</div></a>
+        <a class="next" href="#"><div>下一节</div></a>
+    </div>`
+    const nav = createElementFromHTML(p)
+
+    const docp = doc.parentNode
+
+    docp.insertBefore(nav,doc)
+
+    if(no>=0){
+        if(no==0){
+            document.querySelector("#head .back").href = '/tutorials/index.html'
+            document.querySelector("#head .back div").innerHTML = "返回首页"
+        }else{
+            document.querySelector("#head .back").href = location.pathname.replace(/\d+_(.*)\.html/,`${no-1}_${pages[no-1]}.html`)
+        }
+        if(no!=pages.length-1){
+            document.querySelector("#head .next").href = location.pathname.replace(/\d+_(.*)\.html/,`${no+1}_${pages[no+1]}.html`)
+        }else{
+            document.querySelector("#head .next div").innerHTML = "待更新"
+        }
+    }
 }
